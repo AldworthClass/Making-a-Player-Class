a6545fad66256e4ada7d754ccf6fc29c4e152b54
Steve Aldworth <s.aldworth@tvdsb.ca>
2022-01-25 11:24:21 -0500

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Making_a_Player_Class
+{
+    class Player
+{
+        private Texture2D _texture;
+        private Rectangle _location;
+        private Vector2 _speed;
+
+        public Player(Texture2D texture, int x, int y)
+        {
+            _texture = texture;
+            _location = new Rectangle(x, y, 30, 30);
+            _speed = new Vector2();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _location, Color.White);
+        }
+
+        private void Move()
+        {
+            _location.X += (int)_speed.X;
+            _location.Y += (int)_speed.Y;
+        }
+
+        public void Update()
+        {
+            Move();
+           
+        }
+
+        public Boolean Collide(Rectangle item)
+        {
+            return _location.Intersects(item);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Making_a_Player_Class
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
